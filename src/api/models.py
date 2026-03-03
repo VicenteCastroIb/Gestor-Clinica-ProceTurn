2f75b261a9644ad1118d537442e6749f27e8a442
Vicente Castro Ibarra <vicentecastroibarra@gmail.com>
2026-03-03 00:36:58 +0000

    creacion User db

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,39 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, DateTime
 from sqlalchemy.orm import Mapped, mapped_column
-
+from sqlalchemy.sql import func
+from datetime import datetime
 db = SQLAlchemy()
 
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+class User(db.Model):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    role: Mapped[str] = mapped_column(String(20), nullable=False)
+    full_name: Mapped[str] = mapped_column(String(120), nullable=False)
+    dni: Mapped[int] = mapped_column(nullable=False, unique=True)
+    affiliate_number: Mapped[int] = mapped_column(nullable=False, unique=True)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    password_hash: Mapped[str] = mapped_column(String(30), nullable=False)
+    phone: Mapped[int] = mapped_column(unique=True)
+    status: Mapped[bool] = mapped_column(
+        Boolean(), default=True, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
     def serialize(self):
         return {
             "id": self.id,
+            "role": self.role,
+            "full_name": self.full_name,
+            "dni": self.dni,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "phone": self.phone,
+            "is_active": self.is_active,
+            "created_at": self.created_at.isoformat() if self.created_at else None,
+            "updated_at": self.updated_at.isoformat() if self.updated_at else None
         }

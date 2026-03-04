374eef7398064ef9dca60a25de8381a972ec9902
Laureano <laugx2002@hotmail.com>
2026-03-04 20:28:08 -0300

    add frontend route protection by role

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -9,19 +9,21 @@ import { Single } from "./pages/Single";
 import { Demo } from "./pages/Demo";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 import { Staff } from "./pages/Staff";
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/login" element={<Login />} />
-      <Route path="/" element={<Layout />} errorElement={<h1>Not found!</h1>} >
-        <Route path="/" element={<Home />} />
-        <Route path="/single/:theId" element={<Single />} />
-        <Route path="/demo" element={<Demo />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/staff" element={<Staff />} />
+      <Route path="/" element={<Layout />} errorElement={<h1>Not found!</h1>}>
+        <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+        <Route path="/single/:theId" element={<ProtectedRoute><Single /></ProtectedRoute>} />
+        <Route path="/demo" element={<ProtectedRoute><Demo /></ProtectedRoute>} />
+        <Route path="/signup" element={<ProtectedRoute adminOnly><Signup /></ProtectedRoute>} />
+        <Route path="/staff" element={<ProtectedRoute adminOnly><Staff /></ProtectedRoute>} />
       </Route>
+
     </>
   )
 );

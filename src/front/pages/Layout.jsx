02770e4a08287600f7c3c2a65f5480205dee2f63
Drokko-Dev <jaime.vegah@outlook.com>
2026-03-05 21:38:30 -0300

    feat(auth): centralize JWT logic in utils and update ProtectedRoute and Layout guards

--- src/front/pages/Layout.jsx
+++ src/front/pages/Layout.jsx
@@ -1,12 +1,28 @@
+import { useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { StoreContext } from "../hooks/useGlobalReducer";
 import { Outlet } from "react-router-dom/dist"
 import ScrollToTop from "../components/ScrollToTop"
 import { Navbar } from "../components/Navbar"
 import { Footer } from "../components/Footer"
 import { Sidebar } from "../components/Sidebar"
+import { isTokenExpired } from "../utils";
 import "../styles/layout.css";
 
 // Base component that maintains the navbar and footer throughout the page and the scroll to top functionality.
 export const Layout = () => {
+    const navigate = useNavigate();
+    const { dispatch } = useContext(StoreContext);
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+
+        if (token && isTokenExpired(token)) {
+            console.warn("Token detectado como expirado al cargar Layout.");
+            dispatch({ type: "logout_user" });
+            navigate("/login");
+        }
+    }, [navigate, dispatch]);
     return (
         <ScrollToTop>
             <div className="app-wrapper">
